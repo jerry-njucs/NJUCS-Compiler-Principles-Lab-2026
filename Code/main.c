df77f695a8f498a72fba692d73cc1b4a7d7648da
jerry-njucs <231220060@smail.nju.edu.cn>
2026-05-06 08:28:39 -0700

    修改main 增加总入口

--- Code/main.c
+++ Code/main.c
@@ -2,6 +2,8 @@
 #include "Node.h"
 
 void semantic_check(struct Node* node);
+void generate_intercode(struct Node* node, FILE* out_file);
+
 extern FILE *yyin;
 extern void yyrestart(FILE *input_file);
 extern int yyparse(void);
@@ -11,21 +13,34 @@ extern int lexicalError;
 extern int syntaxError;
 
 int main(int argc, char *argv[]) {
-    if (argc <= 1)
+    if (argc <= 2) {
+        fprintf(stderr, "Usage: %s <input.cmm> <output.ir>\n", argv[0]);
         return 1;
-    FILE* f = fopen(argv[1], "r");
-    if (!f) {
+    }
+
+    FILE* in = fopen(argv[1], "r");
+    if (!in) {
         perror(argv[1]);
         return 1;
     }
-    yyrestart(f);
+
+    FILE* out = fopen(argv[2], "w");
+    if (!out) {
+        perror(argv[2]);
+        fclose(in);
+        return 1;
+    }
+
+    yyrestart(in);
     yyparse();
-    fclose(f);
+    fclose(in);
 
     if (lexicalError == 0 && syntaxError == 0) {
         //printTree(root, 0);
         semantic_check(root);
-        //generate_intercode(root, file);
+        generate_intercode(root, out);
     }
+
+    fclose(out);
     return 0;
 }
